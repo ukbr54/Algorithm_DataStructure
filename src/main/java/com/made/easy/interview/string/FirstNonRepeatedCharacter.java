8bee28305d3ed655bc5af321248553601a1ba66a
Ujjwal Gupta <ujjwal.kbr@gmail.com>
2020-02-04 17:30:13 +0530

    Changes the code to java 8

--- src/main/java/com/made/easy/interview/string/FirstNonRepeatedCharacter.java
+++ src/main/java/com/made/easy/interview/string/FirstNonRepeatedCharacter.java
@@ -4,9 +4,13 @@ package com.made.easy.interview.string;
  * Created by Ujjwal Gupta on Jan,2020
  */
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * find the first non-repeated character
@@ -15,20 +19,12 @@ import java.util.Optional;
 public class FirstNonRepeatedCharacter {
 
     public static Character firstNonRepeatedCharacter(String text){
-        Map<Character,Integer> map = new LinkedHashMap<>(text.length());
+        Map<Character, Integer> map =
+                text.chars()
+                .mapToObj(x -> (char)x)
+                .collect(Collectors.toMap(Function.identity(), x -> 1, (oldValue, newValue) -> oldValue + 1,LinkedHashMap::new));
 
-        char[] chars = text.toCharArray();
-
-        for(int i=0; i<chars.length; i++){
-            if(map.containsKey(chars[i])){
-                Integer value = map.get(chars[i]);
-                map.put(chars[i],++value);
-            }else{
-                map.put(chars[i],1);
-            }
-        }
-
-        Optional<Map.Entry<Character, Integer>> entry = map.entrySet().stream().filter(e -> e.getValue() == 1).findFirst();
+        Optional<Map.Entry<Character, Integer>> entry = map.entrySet().stream().filter(m -> m.getValue() == 1).findFirst();
         return entry.get().getKey();
     }
 
